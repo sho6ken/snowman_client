1a84badeadf4d623efc18780e87bffb1109937c9
Kent <>
2024-06-17 12:44:22 +0800

    feat: 狀態機基礎

---
+++ assets/core/cmpt/state/state_cmpt.ts
@@ -0,0 +1,45 @@
+import { _decorator, Component, Node } from 'cc';
+import { StateCtrl } from './state_ctrl';
+
+const { ccclass, property } = _decorator;
+
+/**
+ * 狀態機基礎
+ */
+@ccclass
+export abstract class StateCmpt extends Component {
+    // 編號
+    abstract get id(): number;
+
+    // 所屬控制器
+    protected _ctrl: StateCtrl = null;
+
+    /**
+     * 初始化
+     * @param ctrl 控制器
+     * @param params 初始化參數
+     */
+    init(ctrl: StateCtrl, ...params: any[]): void {
+        this._ctrl = ctrl;
+    }
+
+    /**
+     * 狀態更新
+     * @param dt 
+     * @summary 當前為此狀態時才會更新
+     */
+    abstract onDraw(dt: number): void;
+
+    /**
+     * 狀態開始
+     * @param params 
+     */
+    abstract onBegin(...params: any[]): Promise<void>;
+
+    /**
+     * 狀態結束
+     */
+    abstract onEnd(): void;
+}
+
+
---
+++ assets/core/cmpt/state/state_ctrl.ts
@@ -0,0 +1,13 @@
+import { _decorator, Component, Node } from 'cc';
+
+const { ccclass, property } = _decorator;
+
+/**
+ * 狀態機控制
+ */
+@ccclass
+export class StateCtrl extends Component {
+    // TODO
+}
+
+
